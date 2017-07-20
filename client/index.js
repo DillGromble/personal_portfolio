6c0198bf01c9f0e0cefa4389c690f6f9e3d9c974
Josh White <whitejosh42@gmail.com>
2017-07-19 21:58:20 -0500

    feat(styling): basic navbar style

--- client/index.js
+++ client/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
 import store from './store'
+
+import Main from './Main'
 
 
 ReactDOM.render(
   <Provider store={ store }>
-    <div>Hello World!</div>
+    <Router history={ browserHistory } >
+      <Route path="/" component={ Main }>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
