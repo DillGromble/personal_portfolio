a723fbcb5495e26dda945354dc6d48bd1438db18
Josh White <whitejosh42@gmail.com>
2017-07-31 13:52:50 -0500

    feat(home): add splash text and styling to home page

--- client/Home/Home.js
+++ client/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './home.scss'
 
-const Home = () => {
-  return (
+const Home = () => (
+  <div className="splash-container">
     <div className="splash" />
-  )
-}
+    <div className="splash-text">
+      <h1>Hi, I'm Josh.</h1>
+      <h1>I'm a full stack web developer, and problem solver.</h1>
+    </div>
+  </div>
+)
 
 export default Home
