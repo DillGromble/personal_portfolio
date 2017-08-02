8586e06673ed21a08e5fc3aaf59c927f7e359521
Josh White <whitejosh42@gmail.com>
2017-08-02 15:43:36 -0500

    feat(splash): add bouncing down arrow

--- client/Home/Home.js
+++ client/Home/Home.js
@@ -8,6 +8,7 @@ const Home = () => (
       <h1>Hi, I'm Josh.</h1>
       <h1>I'm a full stack web developer, and problem solver.</h1>
     </div>
+    <img className="splash-arrow" src="assets/arrow-down-black.png" />
   </div>
 )
 
