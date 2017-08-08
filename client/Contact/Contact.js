4e5089752c183bc5ab87ee21b0ea8dad9abcef2a
Josh White <whitejosh42@gmail.com>
2017-08-08 16:05:42 -0500

    feat: add top row of contact info

--- client/Contact/Contact.js
+++ client/Contact/Contact.js
@@ -4,7 +4,24 @@ import './contact.scss'
 module.exports = ({ visible, exit }) => {
   return (
     <div className={`contact-popup ${visible}`}>
-      <img className="exit" src="assets/x-button.png" onClick={ exit } />
+      <div className="relative-container">
+
+        <img className="profile-pic" src="assets/Contact/profile-pic.jpg" />
+        <img className="exit-btn" src="assets/x-button.png" onClick={exit} />
+
+        <section className="vert-flex">
+          <h1>Josh White</h1>
+          <div className="icon-text">
+            <img className="contact-icon" src="assets/Contact/phone.png" />
+            <p>(615)971-8848</p>
+          </div>
+          <div className="icon-text">
+            <img className="contact-icon" src="assets/Contact/gmail.png" />
+            <p>whitejosh42@gmail.com</p>
+          </div>
+        </section>
+
+      </div>
     </div>
   )
 }
