8636904c0827913abce0f03ac79f8992236dc223
Josh White <whitejosh42@gmail.com>
2017-08-11 15:11:48 -0500

    feat(seed): add basic seed file for projects

---
+++ seed.js
@@ -0,0 +1,38 @@
+const db = require('./server/db')
+const Projects = require('./server/db/models/projects')
+
+const projects = [
+    {
+    title: 'Escape Hatch',
+    start_date: new Date(),
+    completed: new Date(),
+    description: 'Escape Hatch is an npm library / web app for monitoring errors in projects, and providing access to solutions. Upon an error, a link to our server is provided, which presents aggregated and weighted results found from calling the GitHub and Stack Overflow APIs.',
+    technologies: ['Node', 'Express', 'Sequelize', 'Postgres', 'React', 'Redux', 'Passport'],
+    github_repo: 'https://github.com/escape-hatch',
+    presentation: 'https://www.youtube.com/watch?v=8yz3erL_hm0',
+    img: 'assets/Projects/escape-hatch.png'
+  }, {
+    title: 'Lumber Jack\'s CTS',
+    start_date: new Date(),
+    completed: new Date(),
+    description: 'Lumber Jack\'s CTS is a top-down real-time online multiplayer game for 4 people. This was a Hackathon project while I was at Fullstack Academy which won the ‘Nerd Cred’ award for most technical problems solved in a short time due to the complications with managing state and latency across the players.',
+    technologies: ['Phaser.io', 'Socket.io', 'Express'],
+    github_repo: 'https://github.com/DillGromble/LumberJacksCTS',
+    img: 'assets/Projects/lumber-jacks.tiff'
+  }, {
+    title: 'Grace Shopper',
+    start_date: new Date(),
+    completed: new Date(),
+    description: 'A mock e-commerce store specializing in seasonal decorations. This was a short term project to enhance our skills at remote collaboration using tools like GitHub and Waffle.io. \n\nI was responsible for much of the work that went into creating the shopping cart API.',
+    technologies: ['Node', 'Express', 'Sequelize', 'Postgres', 'React', 'Redux', 'Passport'],
+    github_repo: 'https://github.com/DillGromble/grace-shopper',
+    img: 'assets/Projects/grace-shopper.png'
+  }
+]
+
+
+db.sync({ force: true })
+  .then(() => Projects.bulkCreate(projects))
+  .then(() => db.close())
+  .catch(err => `Something went wrong: ${err}`)
+
