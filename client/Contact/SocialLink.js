16f7359cbda09cdfa8351cda0c9799c5492d29f7
Josh White <whitejosh42@gmail.com>
2017-08-11 12:30:10 -0500

    chore(clean): make SocialLink.js more readable

--- client/Contact/SocialLink.js
+++ client/Contact/SocialLink.js
@@ -3,10 +3,7 @@ import React from 'react'
 module.exports = ({ link, img, title }) => (
   <div className="icon-text_bottom">
     <a href={ link } target="_blank" rel="noopener noreferrer">
-      <img
-        className="contact-icon"
-        src={ img }
-      />
+      <img className="contact-icon" src={ img } />
     </a>
     <p>{ title }</p>
   </div>
