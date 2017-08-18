8aab06021d0562f9197cd64ce6489b3091080208
Josh White <whitejosh42@gmail.com>
2017-08-18 16:42:23 -0500

    feat(styling): add grid layout and basic styling for project popup

--- client/Home/Home.js
+++ client/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () =>
       <div className="splash-img" />
       <div className="splash-text home-text">
         <h1>Hi, I'm Josh.</h1>
-        <h1>I'm a full stack web developer, and problem solver.</h1>
+        <h1>I'm a full stack software developer, and problem solver.</h1>
       </div>
       <img
         className="splash-arrow"
--- client/Projects/ProjectPopup.js
+++ client/Projects/ProjectPopup.js
@@ -1,10 +1,43 @@
 import React from 'react'
 
-module.exports = ({ poppedUp, exit }) => (
-  <div className={`disable-bg ${poppedUp}`}>
-    <div className={`popup project-popup ${poppedUp}`}>
-      <h1>Welcome to project popup</h1>
-      <button onClick={ exit }>Exit</button>
+import ProjectLink from '../Contact/SocialLink'
+
+module.exports = props => {
+  console.log(props)
+  return (
+    <div className={`disable-bg ${props.poppedUp}`}>
+      <div className={`popup project-popup ${props.poppedUp}`}>
+        <img
+          className="exit-btn"
+          src="assets/x-button.png"
+          onClick={props.exit}
+        />
+
+        <header className="selected-project-header">
+          <h1>{props.project.title}</h1>
+          <br />
+          <hr />
+        </header>
+
+        <section className="selected-project-grid">
+          {props.project.img && <img className="project-img" src={props.project.img} />}
+          <p>{props.project.description}</p>
+
+          <div className="selected-project-links">
+            <ProjectLink
+              link={props.project.github_repo}
+              img="/assets/Contact/github.png"
+              title="Github"
+            />
+            <ProjectLink
+              link={props.project.deployment}
+              img="/assets/Projects/deployment.png"
+              title="Visit"
+            />
+          </div>
+
+        </section>
+      </div>
     </div>
-  </div>
-)
+  )
+}
