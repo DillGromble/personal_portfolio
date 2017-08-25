63f1884c871e094028e0b47d2e42a3006aa085f2
Josh White <whitejosh42@gmail.com>
2017-08-25 11:21:32 -0500

    fix(home): wording

--- client/Home/Home.js
+++ client/Home/Home.js
@@ -38,7 +38,7 @@ const Home = () =>
         I attended a highly selective coding bootcamp based in NYC called <a href="https://www.fullstackacademy.com/" >Fullstack Academy</a>.  There I learned fundamental computer science and algorithms, full stack JavaScript including Node, Express, Postgres, Sequelize, React, and Redux (to name only a few), as well as CSS(Sass), HTML5, and build tools.  However, the most important thing we were taught was how to be passionate, life long learners who needed nothing but an idea and some documentation to build anything.
       </p>
       <p>
-        I'm currently actively seeking Software Engineering or Web Development roles, preferably that tackle difficult issues and cover a wide variety of technologies.  Taking on new languages, technologies and paradigms has become a hobby of mine, and I would love to spend my working hours doing the same!
+        I'm currently seeking Software Engineering or Web Development roles, preferably that tackle difficult issues and cover a wide variety of technologies.  Taking on new languages, technologies and paradigms has become a hobby of mine, and I would love to spend my working hours doing the same!
       </p>
     </section>
   </div>
